22e704f2c4b1cad100ca0ba9494d1153ba5c71e9
Mihaela Srebcheva <stu1801681001@uni-plovdiv.bg>
2022-06-20 00:03:11 +0300

    Do product service

---
+++ Web/Services/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess;
+using DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Services
+{
+	public class ProductService
+	{
+		private AutoProDbContext _dbContext;
+
+		public ProductService(AutoProDbContext dbContext)
+		{
+			this._dbContext = dbContext;
+		}
+
+		public List<Service> GetServices() => this._dbContext.Services.ToList();
+
+		public Service GetService(string id) => this.GetServices().FirstOrDefault(x => x.Id.ToString() == id);
+	}
+}
